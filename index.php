2a1d8653e196802e2ff61d9699549ea9cfbbde40
handri <handrisaeputra@gmail.com>
2019-08-19 21:06:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Contoh Koneksi Mesin Absensi Mengunakan SOAP Web Service</title></head>
+<body bgcolor="#caffcb">
+
+<H3>Contoh Koneksi Mesin Absensi Mengunakan SOAP Web Service</H3><BR>
+
+<a href="tarik-data.php">Download Log Data</a><BR><BR>
+<a href="clear-data.php">Clear Log Data</a><BR><BR>
+<a href="upload-nama.php">Upload Nama</a><BR><BR>
+
+</body>
+</html>
+
